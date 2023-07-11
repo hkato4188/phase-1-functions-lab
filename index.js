acbf65dfac98a6be72c0a8ab29f134261ca653d5
Hiro Kato <hirokikato1@gmail.com>
2023-07-11 00:41:26 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(customer){
+    let blocks = customer - 42;
+    return blocks < 0 ? (blocks * -1) : blocks;
+}
+
+function distanceFromHqInFeet(customer){
+    return 264 * distanceFromHqInBlocks(customer);
+}
+
+function distanceTravelledInFeet(start, destination){
+    let blocks = start - destination;
+    return blocks < 0 ? (blocks * -264) : blocks * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if(distance < 400){
+        return 0;
+    }else if (distance > 400 && distance < 2000){
+        return (distance - 400) * .02
+    }else if (distance > 2000 && distance < 2500){
+        return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+}
